c867113eddbb4b4a1291b1e1fc35c6672a4221cc
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-07-26 04:30:02 +0200

    A new concept started from scratch.

---
+++ Concept/TypeSystem.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Piewik Project.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the project nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+#
+# Piewik type system. An attempt to reach a type system as close to the TTCN-3's one as possible.
+#
+# A selected approach is to try to implement as many TTCN-3's features (template matching mechanism, subtyping,
+# template restrictions, etc.) inside the type system.
+#
+
+#
+# Exceptions used in Piewik type system.
+#
+class TypeSystemException(Exception):
+    pass
+
+class InvalidTTCN3TypeInCtor(TypeSystemException):
+    pass
+
+class InvalidTTCN3TypeInComparison(TypeSystemException):
+    pass
+
+#
+# Types.
+#
+class TTCN3Type(object):
+    def __init__(self, aValue):
+        self.mValue = aValue
+
+    def __ne__(self, aOther):
+        return not self.__eq__(aOther)
+
+    def value(self):
+        return self.mValue
+
+class TTCN3SimpleType(TTCN3Type):
+    def __init__(self, aValue):
+        TTCN3Type.__init__(self, aValue)
+
+#
+# Simple types.
+#
+class Boolean(TTCN3SimpleType):
+    pass
+
+class Integer(TTCN3SimpleType):
+    def __init__(self, aValue=0):
+        if type(aValue) is not int:
+            raise InvalidTTCN3TypeInCtor
+
+        TTCN3SimpleType.__init__(self, aValue)
+
+    def __eq__(self, aOther):
+        if isinstance(aOther, Integer):
+            return self.mValue == aOther.mValue
+        else:
+            raise InvalidTTCN3TypeInComparison
+
+class Float(TTCN3SimpleType):
+    pass
+
+class Charstring(TTCN3SimpleType):
+    pass
---
+++ Concept/UnitTests/TypeSystem_test.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Piewik Project.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the project nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+import unittest
+
+from Concept.TypeSystem import *
+
+class TypeSystem_Integer(unittest.TestCase):
+
+    #
+    # Successful constructions.
+    #
+    def test_CtorConstructsAProperVariable_WithoutParameter(self):
+        try:
+            Integer()
+        except:
+            self.fail()
+
+    def test_CtorConstructsAProperVariable_WithParameter(self):
+        try:
+            Integer(1)
+        except:
+            self.fail()
+
+    def test_CtorSetsAProperValueIfCalledWithoutParameter(self):
+        self.assertEqual(Integer(), Integer(0))
+
+    #
+    # Unsuccessful constructions.
+    #
+    # TODO: All types.
+    #
+    def test_CtorRaisesAnExceptionIfCalledWithInvalidType_Boolean(self):
+        with self.assertRaises(InvalidTTCN3TypeInCtor):
+            Integer(True)
+
+    def test_CtorRaisesAnExceptionIfCalledWithInvalidType_Float(self):
+        with self.assertRaises(InvalidTTCN3TypeInCtor):
+            Integer(1.0)
+
+    def test_CtorRaisesAnExceptionIfCalledWithInvalidType_String(self):
+        with self.assertRaises(InvalidTTCN3TypeInCtor):
+            Integer("WAX")
+
+    #
+    # Successful matching.
+    #
+    def test_ComparisonReturnsTrueForTwoVariablesWithTheSameValue(self):
+        self.assertTrue(Integer(1) == Integer(1))
+
+    #
+    # Unsuccessful matching.
+    #
+    def test_ComparisonReturnsFalseForTwoVariablesWithDifferentValues(self):
+        self.assertFalse(Integer(1) == Integer(2))
+
+    #
+    # TODO: All types.
+    #
+    def test_ComparisonRaisesAnExceptionIfCalledWithInvalidType_Boolean(self):
+        with self.assertRaises(InvalidTTCN3TypeInComparison):
+            Integer(1) == Boolean(False)
+
+    def test_ComparisonRaisesAnExceptionIfCalledWithInvalidType_Float(self):
+        with self.assertRaises(InvalidTTCN3TypeInComparison):
+            Integer(1) == Float(1.0)
+
+    def test_ComparisonRaisesAnExceptionIfCalledWithInvalidType_Charstring(self):
+        with self.assertRaises(InvalidTTCN3TypeInComparison):
+            Integer(1) == Charstring("WAX")
+
+if __name__ == '__main__':
+    unittest.main()

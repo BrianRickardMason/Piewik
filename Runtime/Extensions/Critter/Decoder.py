fb43221163a68fef6b43e5f99a96e563a4e322b6
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-08-13 16:00:32 +0200

    Cosmetic changes.

--- Runtime/Extensions/Critter/Decoder.py
+++ Runtime/Extensions/Critter/Decoder.py
@@ -30,7 +30,7 @@
 from google.protobuf.internal.containers import RepeatedCompositeFieldContainer
 from google.protobuf.message             import Message
 
-from Piewik.Runtime.Decoder                                                        import Decoder
+from Piewik.Runtime.Decoder                                         import Decoder
 from Piewik.Runtime.Extensions.Critter.Interface.MessageCommon      import *
 from Piewik.Runtime.Extensions.Critter.Interface.Messages_pb2       import *
 from Piewik.Runtime.Extensions.Critter.Interface.Translation        import *

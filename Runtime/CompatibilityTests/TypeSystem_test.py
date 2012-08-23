97350e6525ff3018e441384966b9ff8423405d66
Brian Rickard Mason <brian.rickard.mason@gmail.com>
2012-08-23 05:08:45 +0200

    Compatibility tests updated.

--- Runtime/CompatibilityTests/TypeSystem_test.py
+++ Runtime/CompatibilityTests/TypeSystem_test.py
@@ -29,7 +29,7 @@
 
 import unittest
 
-from Runtime.TypeSystem import *
+from Runtime.NewTypeSystem import *
 
 class TypeSystem_Construction(unittest.TestCase):
     #
@@ -40,10 +40,11 @@ class TypeSystem_Construction(unittest.TestCase):
     # var boolean myVariable;
     #
     def test_Boolean(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
-        myVariable = Boolean(SimpleType())
+        myVariable = Boolean()
 
         #
         # Compatibility.
@@ -57,7 +58,7 @@ class TypeSystem_Construction(unittest.TestCase):
         #
         # TTCN-3.
         #
-        myVariable = Integer(SimpleType())
+        myVariable = Integer()
 
         #
         # Compatibility.
@@ -68,10 +69,11 @@ class TypeSystem_Construction(unittest.TestCase):
     # var float myVariable;
     #
     def test_Float(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
-        myVariable = Float(SimpleType())
+        myVariable = Float()
 
         #
         # Compatibility.
@@ -82,17 +84,18 @@ class TypeSystem_Construction(unittest.TestCase):
     # var charstring myVariable;
     #
     def test_Charstring(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
-        myVariable = Charstring(SimpleType())
+        myVariable = Charstring()
 
         #
         # Compatibility.
         #
         self.assertTrue(isinstance(myVariable, Charstring))
 
-class TypeSystem_Assignment(unittest.TestCase):
+class TypeSystem_AssignmentOfAValueType(unittest.TestCase):
     #
     # TODO: All types.
     #
@@ -101,16 +104,17 @@ class TypeSystem_Assignment(unittest.TestCase):
     # var boolean myVariable := True;
     #
     def test_Boolean(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
-        myVariable = Boolean(SimpleType())
-        myVariable.assign(BooleanValue(True))
+        myVariable = Boolean()
+        myVariable.assignValueType(BooleanValue(True))
 
         #
         # Compatibility.
         #
-        self.assertEqual(myVariable.value(), BooleanValue(True))
+        self.assertEqual(myVariable.valueType(), BooleanValue(True))
 
     #
     # var integer myVariable := 1;
@@ -119,43 +123,45 @@ class TypeSystem_Assignment(unittest.TestCase):
         #
         # TTCN-3.
         #
-        myVariable = Integer(SimpleType())
-        myVariable.assign(IntegerValue(1))
+        myVariable = Integer()
+        myVariable.assignValueType(IntegerValue(1))
 
         #
         # Compatibility.
         #
-        self.assertEqual(myVariable.value(), IntegerValue(1))
+        self.assertEqual(myVariable.valueType(), IntegerValue(1))
 
     #
     # var float myVariable := 1.0;
     #
     def test_Float(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
-        myVariable = Float(SimpleType())
-        myVariable.assign(FloatValue(1.0))
+        myVariable = Float()
+        myVariable.assignValueType(FloatValue(1.0))
 
         #
         # Compatibility.
         #
-        self.assertEqual(myVariable.value(), FloatValue(1.0))
+        self.assertEqual(myVariable.valueType(), FloatValue(1.0))
 
     #
     # var charstring myVariable := "Jochen von Ulm";
     #
     def test_ConstructingVariableWithValueAssigned_Charstring(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
-        myVariable = Charstring(SimpleType())
+        myVariable = Charstring()
         myVariable.assign(CharstringValue("Jochen von Ulm"))
 
         #
         # Compatibility.
         #
-        self.assertEqual(myVariable.value(), CharstringValue("Jochen von Ulm"))
+        self.assertEqual(myVariable.valueType(), CharstringValue("Jochen von Ulm"))
 
 class Typesystem_StructuredTypeDefinition_Record(unittest.TestCase):
     #
@@ -167,7 +173,7 @@ class Typesystem_StructuredTypeDefinition_Record(unittest.TestCase):
         #
         class MyRecord(Record):
             def __init__(self):
-                Record.__init__(self, SimpleType())
+                Record.__init__(self, {})
 
         #
         # Compatibility.
@@ -182,19 +188,20 @@ class Typesystem_StructuredTypeDefinition_Record(unittest.TestCase):
     # };
     #
     def test_NonEmpty(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
         class MyRecord(Record):
             def __init__(self):
-                Record.__init__(self, SimpleType(), {'foo': Integer(SimpleType()), 'bar': Charstring(SimpleType())})
+                Record.__init__(self, {'foo': Integer(), 'bar': Charstring()})
 
         #
         # Compatibility.
         #
         self.assertTrue(issubclass(MyRecord, Record))
 
-class TypeSystem_StructuredTypeDefinitionAndAssignment_Record(unittest.TestCase):
+class TypeSystem_StructuredTypeDefinitionAndAssignmentOfAValueType_Record(unittest.TestCase):
     #
     # type record MyRecord {};
     # var MyRecord myRecordInstance := {};
@@ -205,9 +212,9 @@ class TypeSystem_StructuredTypeDefinitionAndAssignment_Record(unittest.TestCase)
         #
         class MyRecord(Record):
             def __init__(self):
-                Record.__init__(self, SimpleType())
+                Record.__init__(self, {})
         myRecordInstance = MyRecord()
-        myRecordInstance.assign({})
+        myRecordInstance.assignValueType({})
 
         #
         # Compatibility.
@@ -227,36 +234,37 @@ class TypeSystem_StructuredTypeDefinitionAndAssignment_Record(unittest.TestCase)
     # };
     #
     def test_NonEmpty(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
         class MyRecord(Record):
             def __init__(self):
-                Record.__init__(self, SimpleType(), {'field1': Integer(SimpleType()),
-                                                     'field2': Charstring(SimpleType())})
+                Record.__init__(self, {'field1': Integer(),
+                                       'field2': Charstring()})
         myRecordInstance = MyRecord()
-        myRecordInstance.assign({'field1': Integer(SimpleType()).assign(IntegerValue(1)),
-                                 'field2': Charstring(SimpleType()).assign(CharstringValue("Varsovie"))})
+        myRecordInstance.assignValueType({'field1': Integer().assign(IntegerValue(1)),
+                                          'field2': Charstring().assign(CharstringValue("Varsovie"))})
 
         #
         # Compatibility.
         #
         self.assertTrue(isinstance(myRecordInstance, MyRecord))
-        self.assertEqual(myRecordInstance.getField('field1'), Integer(SimpleType()).assign(IntegerValue(1)))
-        self.assertEqual(myRecordInstance.getField('field2'),
-                         Charstring(SimpleType()).assign(CharstringValue("Varsovie")))
+        self.assertEqual(myRecordInstance.getField('field1'), Integer().assign(IntegerValue(1)))
+        self.assertEqual(myRecordInstance.getField('field2'), Charstring().assign(CharstringValue("Varsovie")))
 
 class TypeSystem_Subtyping_TypeAliasing_SimpleType(unittest.TestCase):
     #
     # type boolean MyNewBoolean;
     #
     def test_Boolean(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
         class MyNewBoolean(Boolean):
             def __init__(self):
-                Boolean.__init__(self, SimpleType())
+                Boolean.__init__(self)
 
         #
         # Compatibility.
@@ -272,7 +280,7 @@ class TypeSystem_Subtyping_TypeAliasing_SimpleType(unittest.TestCase):
         #
         class MyNewInteger(Integer):
             def __init__(self):
-                Integer.__init__(self, SimpleType())
+                Integer.__init__(self)
 
         #
         # Compatibility.
@@ -283,12 +291,13 @@ class TypeSystem_Subtyping_TypeAliasing_SimpleType(unittest.TestCase):
     # type float MyNewFloat;
     #
     def test_Float(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
         class MyNewFloat(Float):
             def __init__(self):
-                Float.__init__(self, SimpleType())
+                Float.__init__(self)
 
         #
         # Compatibility.
@@ -299,12 +308,13 @@ class TypeSystem_Subtyping_TypeAliasing_SimpleType(unittest.TestCase):
     # type charstring MyNewCharstring;
     #
     def test_Charstring(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
         class MyNewCharstring(Charstring):
             def __init__(self):
-                Charstring.__init__(self, SimpleType())
+                Charstring.__init__(self)
 
         #
         # Compatibility.
@@ -319,7 +329,7 @@ class TypeSystem_Subtyping_TypeAliasing_StructuredType(unittest.TestCase):
     def test_Record_Empty(self):
         class MyRecord(Record):
             def __init__(self):
-                Record.__init__(self, SimpleType())
+                Record.__init__(self, {})
         class MyNewRecord(MyRecord):
             def __init__(self):
                 MyRecord.__init__(self)
@@ -338,13 +348,14 @@ class TypeSystem_Subtyping_TypeAliasing_StructuredType(unittest.TestCase):
     # type MyRecord MyNewRecord;
     #
     def test_Record_NonEmpty(self):
+        self.skipTest("Not implemented yet.")
         #
         # TTCN-3.
         #
         class MyRecord(Record):
             def __init__(self):
-                Record.__init__(self, SimpleType(), {'field1': Integer(SimpleType()),
-                                                     'field2': Charstring(SimpleType())})
+                Record.__init__(self, {'field1': Integer(),
+                                       'field2': Charstring()})
         class MyNewRecord(MyRecord):
             def __init__(self):
                 MyRecord.__init__(self)
